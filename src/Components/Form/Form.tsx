eb236d1bf54004451112e9911eec71700511ef48
salman <salmannova@gmail.com>
2020-11-17 17:19:38 +0100

    setup structure for the app

---
+++ src/Components/Form/Form.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import styled from "styled-components";
+
+interface FormProps {}
+
+const Form: React.FC<FormProps> = () => {
+  return <FormContainer></FormContainer>;
+};
+
+const FormContainer = styled.div`
+  border-radius: 4px;
+`;
