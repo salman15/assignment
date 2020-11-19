1eef2acd70787c0fef2a27433de7f128997de257
salman <salmannova@gmail.com>
2020-11-19 15:02:22 +0100

    updated snapshots

--- src/Containers/SignUp/SignUp.test.tsx
+++ src/Containers/SignUp/SignUp.test.tsx
@@ -1,24 +1,6 @@
 import React from "react";
 import { render, fireEvent } from "@testing-library/react";
 import SignUp from "./SignUp";
-import axios from "axios";
-import MockAdapter from "axios-mock-adapter";
-
-const mockPost = new MockAdapter(axios, { delayResponse: 0 });
-const mockGet = new MockAdapter(axios, { delayResponse: 0 });
-
-/**
- * Mock both post and get request
- */
-beforeEach(() => {
-  mockPost
-    .onPost("http://localhost:3004/post")
-    .reply(200, { postData: "I'm some post data" });
-
-  mockGet
-    .onGet("http://localhost:3004/delay/4")
-    .reply(200, { postData: "I'm some post data" });
-});
 
 describe("Test the SignUp component", () => {
   it("Test if SignUp component renders with children and input values can be adjusted", () => {
